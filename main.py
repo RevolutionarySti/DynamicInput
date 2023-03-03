c487749d0d6aebb8bf85e900655e46322004e689
zmq23 <zmq23@drexel.edu>
2023-03-03 12:23:23 -0500

    Blue

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 from flask import Flask, render_template
+from flask import request
+import sqlite3
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
 
-@app.route("/")
+
+@app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
-app.run()
+connect = sqlite3.connect('database.db')
+connect.execute(
+	'CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, \
+	email TEXT, city TEXT, country TEXT, phone TEXT)')
+
+
+@app.route('templates\join.html', methods=['GET', 'POST'])
+def join():
+	if request.method == 'POST':
+		name = request.form['name']
+		email = request.form['email']
+		city = request.form['city']
+		country = request.form['country']
+		phone = request.form['phone']
+
+		with sqlite3.connect("database.db") as users:
+			cursor = users.cursor()
+			cursor.execute("INSERT INTO PARTICIPANTS \
+			(name,email,city,country,phone) VALUES (?,?,?,?,?)",
+						(name, email, city, country, phone))
+			users.commit()
+		return render_template("index.html")
+	else:
+		return render_template('join.html')
+
+
+@app.route('templates\participants.html')
+def participants():
+	connect = sqlite3.connect('database.db')
+	cursor = connect.cursor()
+	cursor.execute('SELECT * FROM PARTICIPANTS')
+
+	data = cursor.fetchall()
+	return render_template("participants.html", data=data)
+
+if __name__ == '__main__':
+	app.run(debug=False)
